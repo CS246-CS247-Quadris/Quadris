55a687a75109c0489f7f1596a52d8b41950df403
Samir Musali <samir.musali@logdna.com>
2018-06-11 02:10:28 +0300

    Cleaned

--- grid.d
+++
@@ -1,2 +0,0 @@
-grid.o: grid.cpp grid.h cell.h textdisplay.h window.h block.h level.h \
- level0.h level1.h level2.h level3.h
--- level0.d
+++
@@ -1,2 +0,0 @@
-level0.o: level0.cpp level0.h level.h block.h cell.h textdisplay.h \
- window.h
